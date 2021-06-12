514b3029c0de1dfaf546f72c259b1bb1ab3f8f58
ingduk2 <ingduk2@naver.com>
2021-06-12 14:27:01 +0900

    baekjoon - solvedac - level1 - 고양이

---
+++ src/ps/baekjoon/solvedac/level1/Q10171.java
@@ -0,0 +1,15 @@
+package ps.baekjoon.solvedac.level1;
+
+/**
+ * 고양이
+ * https://www.acmicpc.net/problem/10171
+ */
+public class Q10171 {
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
