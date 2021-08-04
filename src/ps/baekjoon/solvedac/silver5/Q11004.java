e53dbbe680d756383627dab729d6a6998ba39cc0
ingduk2 <ingduk2@naver.com>
2021-08-04 15:35:11 +0900

    baekjoon - solvedac - silver5 - k번째 수

---
+++ src/ps/baekjoon/solvedac/silver5/Q11004.java
@@ -0,0 +1,56 @@
+package ps.baekjoon.solvedac.silver5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * k번째 수
+ * https://www.acmicpc.net/problem/11004
+ */
+public class Q11004 {
+
+    public static void main(String[] args) {
+        Input in = new Input();
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        Arrays.sort(arr);
+        System.out.println(arr[k - 1]);
+    }
+
+    private static class Input {
+        private BufferedReader br;
+        private StringTokenizer st;
+
+        public Input() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public String line() throws IOException {
+            return br.readLine();
+        }
+    }
+}
