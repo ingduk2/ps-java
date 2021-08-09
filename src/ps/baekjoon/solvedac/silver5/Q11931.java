bc8718d8c30164b8bae594db0c9a986c7a626fdd
ingduk2 <ingduk2@naver.com>
2021-08-09 19:18:20 +0900

    baekjoon - solvedac - silver5 - 수 정렬하기4

---
+++ src/ps/baekjoon/solvedac/silver5/Q11931.java
@@ -0,0 +1,60 @@
+package ps.baekjoon.solvedac.silver5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+/**
+ * 수 정렬하기 4
+ * https://www.acmicpc.net/problem/11931
+ */
+public class Q11931 {
+
+    public static void main(String[] args) {
+        Input in = new Input();
+        StringBuilder result = new StringBuilder();
+        int n = in.nextInt();
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        Arrays.sort(arr, Comparator.reverseOrder());
+        for (Integer integer : arr) {
+            result.append(integer).append("\n");
+        }
+
+        System.out.println(result);
+    }
+
+    private static class Input {
+        private BufferedReader br;
+        private StringTokenizer st;
+
+        public Input() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public String line() throws IOException {
+            return br.readLine();
+        }
+    }
+}
