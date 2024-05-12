10b50cd5596b915f30095a8d1d54c0e807aa081b
ingduk2 <ingduk2@naver.com>
2024-05-12 20:02:59 +0900

    [자바 알고리즘 인터뷰 with 코틀린] - leetcode   - 21. Merge Two Sorted Lists

---
+++ src/test/java/ps/java_algorithm_interview_with_kotlin/Q14_MergeTwoSortedLists.java
@@ -0,0 +1,105 @@
+package ps.java_algorithm_interview_with_kotlin;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+/**
+ * https://leetcode.com/problems/merge-two-sorted-lists/
+ * - 2개의 ListNode 를 하나의 sorted List 로 합쳐라
+ *
+ * 1. 재귀 구조로 연결
+ */
+public class Q14_MergeTwoSortedLists {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+
+    private static Stream<Arguments> arguments() {
+        return Stream.of(
+                Arguments.of(
+                        new ListNode(
+                                1, new ListNode(
+                                2, new ListNode(
+                                4))),
+                        new ListNode(
+                                1, new ListNode(
+                                3, new ListNode(
+                                4))),
+                        new ListNode(
+                                1, new ListNode(
+                                1, new ListNode(
+                                2, new ListNode(
+                                3, new ListNode(
+                                4, new ListNode(
+                                4))))))
+                ),
+                Arguments.of(
+                        new ListNode(),
+                        new ListNode(),
+                        new ListNode()
+                ),
+                Arguments.of(
+                        new ListNode(),
+                        new ListNode(0),
+                        new ListNode(0)
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("arguments")
+    void test(ListNode list1, ListNode list2, ListNode expected) {
+        ListNode result = new Q14_MergeTwoSortedLists().mergeTwoLists(list1, list2);
+        printListNode(result);
+        printListNode(expected);
+        assertListNode(result, expected);
+    }
+
+    private void printListNode(ListNode node) {
+        ListNode print = node;
+        while (print != null) {
+            System.out.print(print.val + " ");
+            print = print.next;
+        }
+        System.out.println();
+    }
+
+    private static void assertListNode(ListNode result, ListNode expected) {
+        while (result != null && expected != null) {
+            if (result.val != expected.val) {
+                throw new RuntimeException();
+            }
+            result = result.next;
+            expected = expected.next;
+        }
+    }
+}
