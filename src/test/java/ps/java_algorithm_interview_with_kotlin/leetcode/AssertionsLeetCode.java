500a5d2062ee9ff94f12cc74334e0503e6a1b0ef
ingduk2 <ingduk2@naver.com>
2024-05-14 21:54:57 +0900

    [자바 알고리즘 인터뷰 with 코틀린] - leetcode   - assertListNode : List 로 변환해서 비교하게 변경

--- src/test/java/ps/java_algorithm_interview_with_kotlin/leetcode/AssertionsLeetCode.java
+++ src/test/java/ps/java_algorithm_interview_with_kotlin/leetcode/AssertionsLeetCode.java
@@ -1,15 +1,34 @@
 package ps.java_algorithm_interview_with_kotlin.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AssertionsLeetCode {
     private AssertionsLeetCode() {}
 
     public static void assertListNode(ListNode result, ListNode expected) {
-        while (result != null && expected != null) {
-            if (result.val != expected.val) {
-                throw new RuntimeException();
-            }
+        List<Integer> resultList = new ArrayList<>();
+        List<Integer> expectedList = new ArrayList<>();
+
+        while (result != null) {
+            resultList.add(result.val);
             result = result.next;
+        }
+
+        while (expected != null) {
+            expectedList.add(expected.val);
             expected = expected.next;
+        }
+
+        assertThat(resultList).isEqualTo(expectedList);
+    }
+
+    public static void assertListNode(ListNode result, int[] expected) {
+        for (int e : expected) {
+            assertThat(result.val).isEqualTo(e);
+            result = result.next;
         }
     }
 }
